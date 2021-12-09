65dec13f656e1010f1a8df77364639d29dc89b09
RDLL <rdll00@protonmail.com>
2021-12-08 20:56:42 -0600

    Pedir datos a usuario

--- Quarter_wave_impedance_transformer.m
+++ Quarter_wave_impedance_transformer.m
@@ -34,12 +34,7 @@ end
 plot(0,0, 'X', 'MarkerSize',12);
 
 %Gráfica de impedancia, admitancia y SWR
-%Zl = 25 - 1i*50; %Impedancia de la carga
-%Z0 = 75; %Impedancia caracteristica de la línea de transmisión
-%er = 2.56; %permitividad relativa de la línea
-%fc = 48e6; %Frecuencia de la señal
 C = 3e8; %Velocidad de la luz
-lambda = C/fc*sqrt(er); %longitud de onda
 
 %Pedir Impedancia de la carga
 prompt = 'Ingresa la parte real de la impedancia de la carga Zl: ';
@@ -52,29 +47,39 @@ Zl = re + 1i*im; %Impedancia de la carga
 prompt = 'Ingresa la impedancia caracterisitica de la linea de transmisión Z0: ';
 Z0 = input(prompt); %Impedancia caracteristica de la línea de transmisión
 
-%Pedir permitividad relativa
-prompt = 'Ingresa la permitividad relativa';
+%Pedir Permitividad relativa
+prompt = 'Ingresa la permitividad relativa de la linea de transmisión Er: ';
 er = input(prompt);
 
-%Pedir la frecuencia
-prompt = 'Ingresa la frecuencia';
-fc = input(prompt);
-disp('<a href="https://es.wikipedia.org/wiki/Hercio#M%C3%BAltiplos_del_hercio">Tabla de múltiplos y submúltiplos del SI</a>');
-prompt = 'Ingresa el multiplo de la frecuencia, según la tabla de múltiplos y submúltiplos:';
-m = input(prompt);
-fc = fc*10^m;
+%Pedir frecuencia
+fprintf('Instrucciones para la frecuencia:\n');
+fprintf('Se pide el valor y el múltiplo de la fecuencia por separado\n');
+fprintf('Para 48MHz primero sería 48, después un 6, checar tabla de múltiplos\n')
+disp('<a href = "https://es.wikipedia.org/wiki/Hercio#M%C3%BAltiplos_del_hercio">Tabla</a>');
 
+prompt = 'Ingresa el valor de la frecuencia';
+f=input(prompt);
+
+prompt = 'Ingresa el multiplo de la frecuencia';
+p = input(prompt);
+
+fc = f*10^p;
+
+lambda = C/fc*sqrt(er);
 %Impedancia normalizada
 Zn = Zl/Z0;
 Yn = (Zn - 1)/(Zn + 1);
 plot(real(Yn),imag(Yn), 'o', 'MarkerSize',12);
 
+
+
 %Circulo de impedancia
 rz = abs(Yn);
 xc = linspace(-pi,pi,100);
-yc = linspace(-pi,pi,100);
+%yc = linspace(-pi,pi,100);
 cimp = rz*( cos(xc) + 1i*sin(xc) );
 plot(real(cimp),imag(cimp))
+
 
 %Cálculo SWR
 cimp = rz*( cos(0) + 1i*sin(0));
@@ -91,14 +96,20 @@ else
     longTrans = abs((pi+z)/(4*pi));
     theta2 = pi;
 end
-disp('*** La distancia a la que debe colocarse el transformador es igual a: ')
-disp(['  ' num2str(longTrans) ' lambdas'])
+
+if (real(Yn) <= 0 && imag(Yn) <= 0) || (real(Yn) >= 0 && imag(Yn) <= 0)
+    longTrans = longTrans + 0.250;
+end
+
+disp('*** La distancia a la que debe colocarse el transformador es igual a: ');
+disp(['  ' num2str(longTrans) ' lambdas']);
+disp('Longitud de onda: 'num2str(lambda));
 
 %Impedancia del transformador
 Yt = exp(1i*(theta2) + abs(Yn));
 Zt = (1 + Yt)/(1 - Yt);
 impTrans = sqrt(Zt * Z0^2);
 disp('*** La impedancia del transformador es igual a: ')
-disp(['  ' num2str(impTrans) ' lambdas'])
+disp(['  ' num2str(impTrans)])
 
 hold off
